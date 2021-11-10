115b327138479482e04c5b7466d29f44915d3bce
Jessica Fatta <jessicafatta@gmail.com>
2021-11-10 17:08:38 -0500

    Build classes

---
+++ main.js
@@ -0,0 +1,10 @@
+var classicGameButton = document.querySelector(".classic-game");
+var difficultGameButton = document.querySelector(".difficult-game");
+
+
+classicGameButton.addEventListener("click", playClassic);
+
+
+function playClassic() {
+  
+}
