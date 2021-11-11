46b4ffc384b1fbed579ef0e140f70b2b45af4c12
Jessica Fatta <jessicafatta@gmail.com>
2021-11-10 19:21:31 -0500

    Add classic button function

--- main.js
+++ main.js
@@ -1,10 +1,38 @@
 var classicGameButton = document.querySelector(".classic-game");
 var difficultGameButton = document.querySelector(".difficult-game");
-
+var paperImage = document.querySelector(".paper-image");
+var rocksImage = document.querySelector(".rocks-image");
+var scissorsImage = document.querySelector(".scissors-image");
+var fighterChoice = document.querySelector(".fighter");
+var chooseGameHeadline = document.querySelector(".choose-game-headline");
 
 classicGameButton.addEventListener("click", playClassic);
 
 
+function showEmojis(element) {
+  element.classList.remove("hidden")
+};
+
+function hideEmojis(element) {
+  element.classList.add("hidden")
+};
+
+function hideVisibility(element) {
+  element.classList.add("hidden")
+};
+
+function showVisibility(element) {
+  element.classList.remove("hidden")
+};
+
+
 function playClassic() {
-  
-}
+  hideEmojis(classicGameButton);
+  hideEmojis(difficultGameButton);
+  showEmojis(paperImage);
+  showEmojis(rocksImage);
+  showEmojis(scissorsImage);
+  hideVisibility(chooseGameHeadline);
+  showVisibility(fighterChoice);
+
+};
