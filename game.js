1ec7adcf3143a5f58633275e6cf5f5a93121e2d6
Jessica Fatta <jessicafatta@gmail.com>
2021-11-16 19:50:23 -0500

    Refactor HTML

--- game.js
+++ game.js
@@ -1,37 +1,16 @@
 class Game {
   constructor(type) {
-    this.player = new Player
-
+    this.player = new Player("human", "🙋🏻‍♀️ ");
+    this.computer = new Player("computer", "💻 ");
     this.type = type || "classic";
     this.choices = ["Rocks", "Paper", "Scissors"];
-    // this.choicesDifficult = ["Rocks", "Paper", "Scissors", "Alien", "Lizard"];
-    // this.winner = game.winner;
 
   }
 
   chooseGame(type) {
     this.type = type;
-    if (this.type === "difficult") {
+    if ((this.type === "difficult") && (this.choices < 5)) {
     this.choices.push("Lizard", "Alien")
     }
   }
-
-
-  determineWinner() {
-
-  }
 };
-// this,player = new player (human and girl emoji)
-// this.computer = instatiant new player class called human and computer
-//
-// game takes in a type
-// game takes in type as the only constructor
-// and this.type would be classic || spicy
-//
-// this.choices contains the array of choices
-//
-// this.winner gets reassigned whenever
-//
-// play game method this.type = type if this.type === spicy then push lizard and alien into choice array
-//
-// determine winner if this.playerchoice == computer choice its a draw etc aka the play game function in main
